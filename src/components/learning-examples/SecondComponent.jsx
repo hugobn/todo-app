28aa0998e41016822e552a181b7b00d7843f25b1
Developer <hbenavidesn@outlook.com>
2022-06-11 11:27:20 -0500

    Adding the whole todo-app repository to GitHub

---
+++ src/components/learning-examples/SecondComponent.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+//Class Component
+class SecondComponent extends Component{
+    render(){
+      return(
+        <div className="secondComponent">
+          Second React Component
+        </div>
+      )
+    }
+}
+
+// Best Practice to follow
+export default SecondComponent
---
+++ src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react"
+
+//Function Component
+export default function ThirdComponent(){
+    return(
+      <div className="thirdComponent">
+        Third React Component
+      </div>
+    )
+  }
