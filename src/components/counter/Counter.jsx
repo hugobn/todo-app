900a9ad21ddb01ddbc73a5b140c816b509979c3d
Developer <hbenavidesn@outlook.com>
2022-06-11 12:00:06 -0500

    Use:Update state (counter value), scope of class and using constructor and define functions in a class

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,17 +1,39 @@
+import { render } from "@testing-library/react"
 import React, {Component} from "react"
 import './Counter.css'
 
-function Counter (){
-    return(
-        <div className="counter">
-            <button onClick={increment}>+1</button>
-            <span className="count">0</span>
-        </div>
-    )
+class  Counter extends Component{
+
+    //Define the initial state in a constructor
+    //state => counter 0
+
+    constructor(){
+        super()  // Have to be used always
+        this.state = {
+            counter: 0
+        }
+        this.increment = this.increment.bind(this)
+    }
+
+    render(){
+        return(
+            <div className="counter">
+                <button onClick={this.increment}>+1</button>
+                <span className="count">{this.state.counter}</span>
+            </div>
+        )
+    }
+
+    increment(){  // Update the state - counter++
+        //console.log('increment')
+        //this.state.counter++    // BAD Practice
+        // Best practice is call SetState method
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
 }
 
-function increment(){
-    console.log('increment')
-}
+
 
 export default Counter
