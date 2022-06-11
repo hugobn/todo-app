7ad05b0ba88da0a05e800bc1df7b2853d6963740
Developer <hbenavidesn@outlook.com>
2022-06-11 14:49:26 -0500

    Refactoring the increment and decrement call into the button using outofFunction

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -57,12 +57,12 @@ class  CounterButton extends Component{
     //Define the initial state in a constructor, state => counter 0
     constructor(){
         super()  // Have to be used always
-        this.state = {
+/*        this.state = {
             counter: 0
         }
         this.increment = this.increment.bind(this)
         this.decrement = this.decrement.bind(this)
-
+*/
     }
 
     // Using the outfunction (callbackfn) avoid to use the binding in constructor example: /this.increment = this.increment.bind(this), render = () => {
@@ -70,8 +70,8 @@ class  CounterButton extends Component{
         //const style = {fontSize: "50px"} 
         return(
             <div className="counter">
-                <button onClick={this.increment}>+{this.props.by}</button>
-                <button onClick={this.decrement}>-{this.props.by}</button>
+                <button onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+                <button onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
                 {/*<span className="count"
                 // The best practice is to have all the css style into the independent css file, style={{fontSize: "50px"}} // We can use css here directly wit this structure
                 //style={style}  // we can also use a variable(var, let, const, etc.) to apply the css style
@@ -80,7 +80,7 @@ class  CounterButton extends Component{
         )
     }
 
-    // Becuase we changed the render function into a outfunct we need to change the increment func into a outfucnt as well, increment = () => {  // Update the state - counter++
+/*    // Becuase we changed the render function into a outfunct we need to change the increment func into a outfucnt as well, increment = () => {  // Update the state - counter++
     increment () {  // Update the state - counter++
         //console.log('increment')
         //this.state.counter++    // BAD Practice
@@ -97,6 +97,7 @@ class  CounterButton extends Component{
         })
         this.props.decrementMethod(this.props.by)
     }
+*/
 
 }
 
