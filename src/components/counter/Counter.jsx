64c658b71d8dbfdb1723ebace789ae6135a6a210
Developer <hbenavidesn@outlook.com>
2022-06-11 13:25:52 -0500

    Moving the state value from the child(CounterButton) to the parent(Counter) component

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -4,22 +4,36 @@ import React, {Component} from "react"
 import './Counter.css'
 
 class Counter extends Component{
+    //Define the initial state in a constructor, state => counter 0
+    constructor(){
+        super() 
+        this.state = {
+            counter: 0
+        }
+        this.increment = this.increment.bind(this)
+    }
+
     render(){
         return(
           <div className="counter">
-            <CounterButton by={1}/>
-            <CounterButton by={5}/>
-            <CounterButton by={10}/>
+            <CounterButton by={1} incrmentMethod={this.increment}/>
+            <CounterButton by={5} incrmentMethod={this.increment}/>
+            <CounterButton by={10} incrmentMethod={this.increment}/>
+            <span className="count">{this.state.counter}</span>
           </div>
         )
       }
+
+    increment (by) {
+        //console.log(`increment from child - ${by}`)
+        this.setState({
+            counter: this.state.counter + by
+        })
+    }
 }
 
 class  CounterButton extends Component{
-
-    //Define the initial state in a constructor
-    //state => counter 0
-
+    //Define the initial state in a constructor, state => counter 0
     constructor(){
         super()  // Have to be used always
         this.state = {
@@ -28,24 +42,21 @@ class  CounterButton extends Component{
         this.increment = this.increment.bind(this)
     }
 
-    // Using the outfunction (callbackfn) avoid to use the binding in constructor example: /this.increment = this.increment.bind(this)
-    //render = () => {
+    // Using the outfunction (callbackfn) avoid to use the binding in constructor example: /this.increment = this.increment.bind(this), render = () => {
     render () {
         //const style = {fontSize: "50px"} 
         return(
             <div className="counter">
                 <button onClick={this.increment}>+{this.props.by}</button>
-                <span className="count"
-                // The best practice is to have all the css style into the independent css file
-                //style={{fontSize: "50px"}} // We can use css here directly wit this structure
+                {/*<span className="count"
+                // The best practice is to have all the css style into the independent css file, style={{fontSize: "50px"}} // We can use css here directly wit this structure
                 //style={style}  // we can also use a variable(var, let, const, etc.) to apply the css style
-                >{this.state.counter}</span>
+                >{this.state.counter}</span>*/}
             </div>
         )
     }
 
-    // Becuase we changed the render function into a outfunct we need to change the increment func into a outfucnt as well
-    // increment = () => {  // Update the state - counter++
+    // Becuase we changed the render function into a outfunct we need to change the increment func into a outfucnt as well, increment = () => {  // Update the state - counter++
     increment () {  // Update the state - counter++
         //console.log('increment')
         //this.state.counter++    // BAD Practice
@@ -53,6 +64,7 @@ class  CounterButton extends Component{
         this.setState({
             counter: this.state.counter + this.props.by
         })
+        this.props.incrmentMethod(this.props.by)
     }
 }
 
