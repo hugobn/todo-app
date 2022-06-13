ffd0d77e6d2dadcbc7c423fa5eb593bea8c37e50
Developer <hbenavidesn@outlook.com>
2022-06-13 18:59:53 -0500

    Styling the header and menu using bootstrap

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -31,9 +31,19 @@ class TodoApp extends Component{
 class HeaderComponent extends Component{
     render(){
         return(
-            <div>
-                Header <hr/>
-            </div>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="http://www.in28minutes.com" className="navbar-brand">in28minutes</a></div>
+                    <ul className="navbar-nav">
+                        <li><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>
+                        <li><Link className="nav-link" to="/todos">Todos</Link></li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li><Link className="nav-link" to="/login">Login</Link></li>
+                        <li><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
+            </header>
         )
     }
 }
