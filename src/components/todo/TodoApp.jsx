e3676fa2a50c12d8e1e13e7ef77bd9ea41f30df6
Developer <hbenavidesn@outlook.com>
2022-06-13 18:35:52 -0500

    Created the floe from login to todos list with a link and we add more options

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import withNavigation from "./WithNavigation";
 import withParams from "./withParams";
 
@@ -37,9 +37,9 @@ class ListTodosComponent extends Component{
         this.state = {
             todos:
                 [ 
-                    {id: 1, description: 'Learn to Dance'},
-                    {id: 2, description: 'Become an Expert at React'},
-                    {id: 3, description: 'Visit India'}
+                    {id: 1, description: 'Learn to Dance', done: false, targetDate: new Date()},
+                    {id: 2, description: 'Become an Expert at React', done: false, targetDate: new Date()},
+                    {id: 3, description: 'Visit India', done: false, targetDate: new Date()}
                 ]
         }
     }
@@ -51,8 +51,10 @@ class ListTodosComponent extends Component{
                 <table>
                     <thead>
                         <tr>
-                            <th>id</th>
-                            <th>description</th>
+                            <th>Id</th>
+                            <th>Description</th>
+                            <th>Done</th>
+                            <th>Is Completed</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -62,6 +64,8 @@ class ListTodosComponent extends Component{
                                 <tr>
                                     <td>{todo.id}</td>
                                     <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toString()}</td>
                                 </tr>
                                 )
                         }
@@ -75,7 +79,9 @@ class ListTodosComponent extends Component{
 class WelcomeComponent extends Component{
     render(){
         return(
-            <div>Welcome {this.props.params.name}</div>
+            <div>
+                Welcome {this.props.params.name}. You can manage your todos <Link to="/todos">here</Link>
+            </div>
         )
     }
 }
