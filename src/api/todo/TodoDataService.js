205b19332c9df0fd9dd6935743523934e3fc42da
Developer <hbenavidesn@outlook.com>
2022-06-17 17:50:45 -0500

    Adding the delete Todo feature to React Frontend

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -5,6 +5,10 @@ class TodoDataService {
     retrieveAllTodos(name){
         return axios.get(`http://localhost:9090/users/${name}/todos`)
     }
+    deleteTodoById(name, id){
+        return axios.delete(`http://localhost:9090/users/${name}/todos/${id}`)
+    }
+
 }
  
 export default new TodoDataService();
--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -13,8 +13,11 @@ class ListTodosComponent extends Component{
                     {id: 2, description: 'Become an Expert at React', done: false, targetDate: new Date()},
                     {id: 3, description: 'Visit India', done: false, targetDate: new Date()}
 */
-                ]
+                ],
+            message: null
         }
+        this.deleteTodoClicked = this.deleteTodoClicked.bind(this)
+        this.refreshTodos = this.refreshTodos.bind(this)
     }
 
     componentWillUnmount(){
@@ -31,6 +34,10 @@ class ListTodosComponent extends Component{
 
     componentDidMount(){
         console.log('componenDidMount')
+        this.refreshTodos()
+    }
+
+    refreshTodos(){
         let username = AuthenticationService.getLoggedInUserName()
         TodoDataService.retrieveAllTodos(username)         //make the call to the API
         .then(
@@ -45,11 +52,26 @@ class ListTodosComponent extends Component{
         )
     }
 
+    deleteTodoClicked(id){
+        let username = AuthenticationService.getLoggedInUserName()
+        //console.log("user:" + username + ", id:" + id)
+        TodoDataService.deleteTodoById(username, id)
+        .then(
+            response => {
+                this.setState({
+                    message: `Delete of todo ${id} successful`
+                })
+                this.refreshTodos()
+            }
+        )
+    }
+
     render(){
         console.log('render')
         return(
             <div>
                 <h1>List Todos</h1>
+                {this.state.message && <div className='alert alert-success'>{this.state.message}</div>}
                 <div className="container">
                     <table className="table">
                         <thead>
@@ -57,6 +79,7 @@ class ListTodosComponent extends Component{
                                 <th>Description</th>
                                 <th>Done</th>
                                 <th>Is Completed</th>
+                                <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -67,6 +90,7 @@ class ListTodosComponent extends Component{
                                         <td>{todo.description}</td>
                                         <td>{todo.done.toString()}</td>
                                         <td>{todo.targetDate.toString()}</td>
+                                        <td><button className='btn btn-warning' onClick={() => {this.deleteTodoClicked(todo.id)}}>Delete</button></td>
                                     </tr>
                                     )
                             }
